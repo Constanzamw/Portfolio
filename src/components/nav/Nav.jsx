96542265209c5f3adaaba0e63124ac74683bc006
Constanzamw <cweiner92@gmail.com>
2024-01-06 14:05:16 -0300

    fin portfolio

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,45 +11,43 @@ import React, { useEffect, useState } from "react";
 // import { useRouter } from "next/navigation";
 
 export default function Nav() {
-
   return (
-    <nav className="bg-transparent py-8 px-8 text-artistfont mb-[1px] bg-red-700">
-      <ul className="flex flex-col sm:flex-row  items-center justify-center sm:justify-between gap-8">
-        <div className="text-center sm:flex sm:justify-center">
+    <nav className="bg-transparent py-4 px-4 md:py-8 md:px-8 text-artistfont mb-[1px] bg-red-700">
+      <ul className="flex flex-col md:flex-row items-center justify-center md:justify-between gap-8">
+        <div className="text-center md:flex md:justify-center">
           <li>
             <Link href="/">
-              <span className="font-rocksalt text-4xl hover:text-pink-600 hover:border-pink-600 ">
+              <span className="font-rocksalt text-md lg:text-4xl hover:text-pink-600 hover:border-pink-600 border-b-[2px]">
                 Constanza Weiner
-           
               </span>
             </Link>
           </li>
         </div>
-        <div className="flex items-center gap-x-8">
+        <div className="flex items-center gap-x-4 md:gap-x-8">
           <li>
             <Link href="/education">
-              <span className="hover:text-pink-600 hover:border-pink-600 pb-1 font-newrocker text-[19px] border-b-[2px] border-gray-200">
+              <span className="hover:text-pink-600 hover:border-pink-600 pb-1 font-newrocker text-[14px] md:text-[19px] border-b-[2px] border-gray-200 border border-1px px-2 md:px-4 py-1 md:py-2 rounded-md">
                 Education
               </span>
             </Link>
           </li>
           <li>
             <Link href="/experience">
-              <span className=" hover:text-pink-600 cursor-pointer hover:border-pink-600 pb-1 font-newrocker text-[19px] border-b-[2px]  border-gray-200">
+              <span className="hover:text-pink-600 cursor-pointer hover:border-pink-600 pb-1 font-newrocker text-[14px] md:text-[19px] border-b-[2px] border border-1px px-2 md:px-4 py-1 md:py-2 rounded-md border-gray-200">
                 Experience
               </span>
             </Link>
           </li>
           <li>
             <Link href="/about">
-              <span className=" hover:text-pink-600 cursor-pointer hover:border-pink-600 pb-1 font-newrocker text-[19px] border-b-[2px]  border-gray-200">
+              <span className="hover:text-pink-600 cursor-pointer hover:border-pink-600 pb-1 font-newrocker text-[14px] md:text-[19px] border-b-[2px] border border-1px px-2 md:px-4 py-1 md:py-2 rounded-md border-gray-200">
                 About me
               </span>
             </Link>
           </li>
           <li>
             <Link href="/contact">
-              <span className=" hover:text-pink-600 cursor-pointer hover:border-pink-600 pb-1 font-newrocker text-[19px] border-b-[2px]  border-gray-200">
+              <span className="hover:text-pink-600 cursor-pointer hover:border-pink-600 pb-1 font-newrocker text-[14px] md:text-[19px] border-b-[2px] border border-1px px-2 md:px-4 py-1 md:py-2 rounded-md border-gray-200">
                 Contact me
               </span>
             </Link>
